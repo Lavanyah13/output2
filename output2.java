a4f90dfa36812c7e53fc8a0bbd4156f24ef98d64
Lavanyah B <104270776+Lavanyah13@users.noreply.github.com>
2023-01-21 22:57:10 +0530

    Create output2.java

---
+++ output2.java
@@ -0,0 +1,10 @@
+class Student{
+    int roll_no;
+    String name;
+}
+class Test {
+        public static void main (String[] args) {
+        Student s=new Student();
+        System.out.println(s);
+    }
+}
